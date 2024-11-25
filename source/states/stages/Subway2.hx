a47f4e3ef4df3e8093e591bb492385c826ce8c93
Justudude321 <justsomedud321@gmail.com>
2024-11-25 17:28:18 -0500

    Fixed train doors, kinda, with really dumb looking code

--- source/states/stages/Subway2.hx
+++ source/states/stages/Subway2.hx
@@ -8,6 +8,7 @@ class Subway2 extends BaseStage
 	// you might have to rename some variables if they're missing, for example: camZooming -> game.camZooming
 
 	var trainGroup:FlxSpriteGroup;
+	var doorArray:Array<FlxSprite> = [];
 	var doors:BGSprite;
 	override function create()
 	{
@@ -60,8 +61,12 @@ class Subway2 extends BaseStage
 	override function update(elapsed:Float)
 	{
 		// Code here
-		var songPos:Float = Conductor.songPosition/1000;
-		camHUD.angle = 2 * Math.sin(songPos);
+		if(curStep < 775){
+			var songPos:Float = Conductor.songPosition/1000;
+			camHUD.angle = 2 * Math.sin(songPos);
+		}
+		else
+			camHUD.angle = 0;
 	}
 
 	override function beatHit()
@@ -71,34 +76,79 @@ class Subway2 extends BaseStage
 	}
 
 	// For events
-	var chance:Int = FlxG.random.int(1, 6);
+	var track:Int = 0;
+	var tracker:Array<Int> = [];
+	override function eventPushedUnique(event:objects.Note.EventNote)
+	{
+		// used for preloading assets used on events that doesn't need different assets based on its values
+		switch(event.event)
+		{	
+			case 'Subway Train': // this is dumb code but fuck it
+				var range:Int = FlxG.random.int(1, Std.parseInt(event.value1));
+				if(range == track){
+					range = ((range + 1) % 7 == 0) ? 7 : range + 1;
+				}
+				track = range;
+				tracker.push(track);
+				// trace("track's value is now " + track);
+
+				var doorExists:Int = 0;
+				for (_door in doorArray) {
+					doorExists |= (_door.ID == track ? 1 : 0);
+				}
+				if (doorExists < 1) {
+					var door:FlxSprite = new FlxSprite();
+					door.ID = track;
+					switch (track) { //precaches doors???
+						case 1:
+							door.setPosition(2171, 58);
+							door.frames = Paths.getSparrowAtlas("doors1");
+						case 2:
+							door.setPosition(2486, 89);
+							door.frames = Paths.getSparrowAtlas("doors2");
+						case 3:
+							door.setPosition(2640, 158);
+							door.frames = Paths.getSparrowAtlas("doors3");
+						case 4:
+							door.setPosition(2485, 91);
+							door.frames = Paths.getSparrowAtlas("doors4");
+						case 5:
+							door.setPosition(2485, 91);
+							door.frames = Paths.getSparrowAtlas("doors5");
+						case 6:
+							door.setPosition(2485, 91);
+							door.frames = Paths.getSparrowAtlas("doors6");
+						case 7:
+							door.setPosition(2485, 91);
+							door.frames = Paths.getSparrowAtlas("doors7");
+					}
+					door.animation.addByPrefix('open', 'DOORS OPENING', 24, false);
+					door.animation.addByPrefix('close', 'DOORS CLOSING', 24, false);
+					door.animation.addByIndices('idle', 'DOORS OPENING', [0], "", 24, false);
+					door.animation.play('idle', true);
+					door.scale.set(1.2, 1.2);
+					door.updateHitbox();
+					doorArray.push(door);
+					trainGroup.insert(0, door);
+					// trace("door" + track + " precached");
+				}
+		}
+	}
+
 	override function eventCalled(eventName:String, value1:String, value2:String, flValue1:Null<Float>, flValue2:Null<Float>, strumTime:Float)
 	{
 		switch(eventName)
 		{
 			case "Subway Train":
-				switch (chance) {//"Borrowed" from a repository I found of V1 I think, sorry Rechi
-					case 1:
-						doors = new BGSprite('doors1', 2171, 58, ['DOORS OPENING']);
-					case 2:
-						doors = new BGSprite('doors2', 2486, 89, ['DOORS OPENING']);
-					case 3:
-						doors = new BGSprite('doors3', 2640, 158, ['DOORS OPENING']);
-					case 4:
-						doors = new BGSprite('doors4', 2485, 91, ['DOORS OPENING']);
-					case 5:
-						doors = new BGSprite('doors5', 2485, 91, ['DOORS OPENING']);
-					case 6:
-						doors = new BGSprite('doors6', 2485, 91, ['DOORS OPENING']);
+				//"Borrowed" from a repository I found of V1 I think, sorry Rechi :(
+				var doors:FlxSprite = null;
+				for (spr in doorArray) {
+					if (spr.ID == tracker[0]) doors = spr;
+					spr.alpha = 0;
 				}
-				doors.animation.addByPrefix('open', 'DOORS OPENING', 24, false);
-				doors.animation.addByPrefix('close', 'DOORS CLOSING', 24, false);
-				doors.animation.addByIndices('idle', 'DOORS OPENING', [0], "", 24, false);
-				doors.animation.play('idle', true);
-				doors.scale.set(1.2, 1.2);
-				doors.updateHitbox();
-				trainGroup.insert(0, doors);
-				trainGroup.x = -6100;
+				if (doors == null) return;
+				doors.alpha = 1;
+				// trace("Summon train" + tracker[0]);
 				var tween1:FlxTween = FlxTween.tween(trainGroup, {x: -2100}, 5, {ease: FlxEase.cubeInOut,
 					onComplete: function(_){
 						var timer1:FlxTimer = new FlxTimer().start(0.75, function(_) {
@@ -107,14 +157,13 @@ class Subway2 extends BaseStage
 								if (name == "open") {
 									var timer2:FlxTimer = new FlxTimer().start(1, function(_) {
 										doors.animation.play('close');
-										chance = ((chance + 1) % 6 == 0) ? 1 : chance + 1;
 									});
 								} else if (name == "close") {
 									var timer3:FlxTimer = new FlxTimer().start(0.75, function(_) {
 										var tween2:FlxTween = FlxTween.tween(trainGroup, {x: 3500}, 5, {ease: FlxEase.cubeInOut,
 											onComplete: function(_){
 												trainGroup.x = -6100;
-												doors.destroy();
+												tracker[0] = tracker[1];
 											}
 										});
 									});
@@ -123,17 +172,6 @@ class Subway2 extends BaseStage
 						});
 					}
 				});
-		}
-	}
-	override function eventPushed(event:objects.Note.EventNote)
-	{
-		// used for preloading assets used on events that doesn't need different assets based on its values
-		switch(event.event)
-		{
-			case "Subway Train":
-				Paths.image('doors' + chance);
-				trace("Precached door " + chance);
-				// No clue if this does anything or not
 		}
 	}
 }
